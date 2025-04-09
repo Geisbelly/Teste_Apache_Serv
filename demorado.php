44affb7b84d0f29d4c8a59f0ac8bb02f2fe45c79
Geisbelly Victória <90412629+Geisbelly@users.noreply.github.com>
2025-04-09 17:07:00 -0300

    base

---
+++ demorado.php
@@ -0,0 +1,5 @@
+<?php
+// Simula um processo demorado
+sleep(50); // espera 70 segundos
+echo "Processo demorado concluído - PHP";
+?>
---
+++ excecao.php
@@ -0,0 +1,4 @@
+<?php
+// Gera uma exceção de runtime
+throw new Exception("Erro de execução - PHP");
+?>
